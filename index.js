2964276f076da92e3e5dfa19eb2a6992d64bcf74
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-01-21 18:27:53 +0900

    babel & nodemon setup

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
-}
 
-// const handleListening = () => {};
+const handleHome = (req, res) => res.send("Hi from Home!");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
