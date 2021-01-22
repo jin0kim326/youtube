de5f57e65ccacbc3f84303e549e63d7cbd4a4ba1
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-01-22 19:55:11 +0900

    make a controllers

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
+import routes from "./routes";
 const app = express();
 
 app.use(helmet());
@@ -14,5 +14,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
