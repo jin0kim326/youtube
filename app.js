be97828dfc0d77763e9271fe91caa4adec2cccde
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-01-22 17:38:27 +0900

    Separation routers

--- app.js
+++ app.js
@@ -3,23 +3,16 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { router } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("Hi from Home!dd");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-const handleRedirect = (req, res, next) => res.redirect("/");
 
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.get("/", handleHome);
-app.get("/profile", handleRedirect, handleProfile);
-app.use("/user", router);
 
 export default app;
