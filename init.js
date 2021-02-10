3bb9e33b4278c36ef052e53a4031308917da1e3c
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-02-10 15:53:06 +0900

    style footer & login page

--- init.js
+++ init.js
@@ -1,9 +1,10 @@
 import app from "./app";
 import "./db";
 import dotenv from "dotenv";
-dotenv.config();
 import "./models/Video";
 import "./models/Comment";
+
+dotenv.config();
 
 const PORT = process.env.PROT || 4000;
 
--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddlewares = (req, res, next) => {
   res.locals.siteName = "YouTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 333,
   };
   next();
