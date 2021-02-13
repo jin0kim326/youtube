9468973b76a5eacab136412cf98b10b9ce68e884
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-02-13 12:46:40 +0900

    session store on mongo & basic privacy

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
+import "./passport";
+import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import passport from "passport";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 import { localsMiddlewares } from "./middlewares";
 
-import "./passport";
+dotenv.config();
 
 const app = express();
+
+const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser());
 
 app.use(localsMiddlewares);
 
--- init.js
+++ init.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
 import app from "./app";
 import "./db";
+
+dotenv.config();
+
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
-dotenv.config();
-
-const PORT = process.env.PROT || 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(` ✅ Listening on : http://localhost:${PORT}`);
