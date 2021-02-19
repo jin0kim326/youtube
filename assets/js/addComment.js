a1a8fcf114ed4d84b3a146a5cd9b64e4fc7ba406
김진영 <53967482+jin0kim326@users.noreply.github.com>
2021-02-20 01:55:22 +0900

    adding a comment #3 (fake realtime)

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,6 +1,21 @@
 import axios from "axios";
 
 const addCommentForm = document.querySelector("#jsAddComment");
+const commentList = document.querySelector("#jsCommentList");
+const commentNumber = document.querySelector("#jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const addComment = (comment) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendComment = async (comment) => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -11,7 +26,9 @@ const sendComment = async (comment) => {
       comment,
     },
   });
-  console.log(response);
+  if (response.status === 200) {
+    addComment(comment);
+  }
 };
 
 const handleSubmit = (event) => {
